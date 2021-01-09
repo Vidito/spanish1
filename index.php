9f6d62bbe5e1eaf6f645dece5fe062b132945e72
Vahid <56190096+Vidito@users.noreply.github.com>
2021-01-09 21:11:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,55 @@
 <?php
-//    for spanish characters
-ini_set('default_charset', 'windows-1252');
 
-$word = explode("-", file_get_contents('sentences.txt'));
-$random = array_rand($word);
+
+
+if(isset($_GET['toss'])) {
+    $number1 = rand(1,6);
+    $number2 = rand(1,6);
+}
+elseif(isset($_GET['reset'])) {
+    header("Location: http://localhost/1/dice.php");
+}
+
+
+
+
 ?>
-<!DOCTYPE html>
-<html lang="es">
 
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- for Spanish characters -->
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
-    <title>Spanish Sentences</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap');
-
-        body {
-            background-image: linear-gradient(90deg, rgb(223, 90, 189), rgb(140, 2, 153));
-            background-repeat: no-repeat;
-            height: 100vh;
-            overflow: hidden;
-            color: white;
-            text-align: center;
-            font-family: 'Balsamiq Sans', cursive;
-        }
-
-
-        h1 {
-            margin: 100px auto;
-            font-size: 45px;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Dice</title>
 </head>
-
 <body>
-    <h2>Random Spanish sentences with the verb 'dar'</h2>
-    <h1> <?php echo $word[$random]; ?></h1>
+<div class="container text-light mt-5">
+            <div class="row justify-content-md-center">
+                <div class="col-md-5 bg-dark p-3">
+                    <h3 class="text-center">Welcome to the random dice tossing</h3>
+                    <h5 class="text-center">Click on the button to see your dice number</h5>
+
+                <div class="text-center mt-3 pt-3">
+                    <img src="images/<?php echo $number1.'.png'; ?>" alt="">
+                    <img src="images/<?php echo $number2.'.png'; ?>" alt="">
+                </div>
+
+                <div class="text-center mt-3 pt-3">
+                <form action="dice.php" method="GET">
+                    <button type="submit" name="toss" class="btn btn-primary btn-block ">Toss the dice</button>
+                    <button type="submit" name="reset" class="btn btn-primary btn-block ">Reset</button>
+                    </form>
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    
+
+
 
 </body>
-
 </html>
