67be2ec3f8fdaad04653ca96152eec320141c950
Vahid <56190096+Vidito@users.noreply.github.com>
2021-01-10 18:08:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,36 +2,62 @@
 if(isset($_GET['r1'])) {
     $ra1=1;
 }
+elseif(isset($_GET['s1'])) {
+    $s1=1;
+}
 elseif(isset($_GET['r2'])) {
     $r2=2;
+}
+elseif(isset($_GET['s2'])) {
+    $s2=2;
 }
 elseif(isset($_GET['r3'])) {
     $r3=3;
 }
+elseif(isset($_GET['ss'])) {
+    $s3=3;
+}
 elseif(isset($_GET['r4'])) {
     $r4=4;
+}
+elseif(isset($_GET['s4'])) {
+    $s4=4;
 }
 elseif(isset($_GET['r5'])) {
     $r5=5;
 }
+elseif(isset($_GET['s5'])) {
+    $s5=5;
+}
 elseif(isset($_GET['r6'])) {
     $r6=6;
+}
+elseif(isset($_GET['s6'])) {
+    $s6=6;
 }
 elseif(isset($_GET['r7'])) {
     $r7=7;
 }
+elseif(isset($_GET['s7'])) {
+    $s7=7;
+}
 elseif(isset($_GET['r8'])) {
     $r8=8;
 }
+elseif(isset($_GET['s8'])) {
+    $s8=8;
+}
 elseif(isset($_GET['r9'])) {
     $r9=9;
+}
+elseif(isset($_GET['s9'])) {
+    $s9=9;
 }
 
 else {
     echo "";
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,7 +92,17 @@ else {
                     <button type="submit" name="r7" class="btn btn-primary btn-block ">Riddle 7</button>
                     <button type="submit" name="r8" class="btn btn-primary btn-block ">Riddle 8</button>
                     <button type="submit" name="r9" class="btn btn-primary btn-block ">Riddle 9</button>
-                    
+                    <p>Solutions</p>
+
+                    <button type="submit" name="s1" class="btn btn-primary btn-block ">Solution 1</button>
+                    <button type="submit" name="s2" class="btn btn-primary btn-block ">Solution 2</button>
+                    <button type="submit" name="s3" class="btn btn-primary btn-block ">Solution 3</button>
+                    <button type="submit" name="s4" class="btn btn-primary btn-block ">Solution 4</button>
+                    <button type="submit" name="s5" class="btn btn-primary btn-block ">Solution 5</button>
+                    <button type="submit" name="s6" class="btn btn-primary btn-block ">Solution 6</button>
+                    <button type="submit" name="s7" class="btn btn-primary btn-block ">Solution 7</button>
+                    <button type="submit" name="s8" class="btn btn-primary btn-block ">Solution 8</button>
+                    <button type="submit" name="s9" class="btn btn-primary btn-block ">Solution 9</button>
                     
                    
                     </form>
@@ -82,6 +118,15 @@ else {
                 <img src="<?php echo $r7.'.jpg';?>" alt=""> 
                 <img src="<?php echo $r8.'.jpg';?>" alt=""> 
                 <img src="<?php echo $r9.'.jpg';?>" alt=""> 
+                <img src="<?php echo $s1.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s2.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s3.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s4.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s5.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s6.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s7.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s8.'1.jpg';?>" alt=""> 
+                <img src="<?php echo $s9.'1.jpg';?>" alt="">
                 </div>
 
                 </div>
